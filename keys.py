99da386bf342d51bb116a1eac48559b21bfafd9d
Hem <66408922+icecrac34r@users.noreply.github.com>
2022-02-02 20:03:43 +0530

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,7 @@
+#Get your keys from twitter developers tools
+#Make sure to give read and write permission to the access keys 
+
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
